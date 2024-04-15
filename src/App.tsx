b43c87c513f779312d14bb0d4170af01182d810c
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2024-04-15 11:47:13 +0300

    Added submit functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input } from './components/input.styles';
 import { SubmitButton } from './components/submitButton.styles';
 import { Container } from './components/container.styles';
 import { Heading } from './components/heading.styles';
 
 const App = () => {
+  const [message, setMessage] = useState<string>('');
+
+  const onSubmit = () => {
+    
+  };
+
   return (
     <section>
       <Container>
         <Heading>Write message to display on LCD</Heading>
-        <Input type='text' placeholder='Write message' />
-        <SubmitButton type='submit' value='Display' />
+        <Input type='text' placeholder='Write message' onChange={(e) => setMessage(e.target.value)} />
+        <SubmitButton type='submit' value='Display' onClick={onSubmit} />
       </Container>
     </section>
   );
