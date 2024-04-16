498a7ec4bdad1d3b6bfa0f6a9da7aeee3a556d90
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2024-04-16 08:23:25 +0300

    Improvements to backend api

--- api/index.js
+++ api/index.js
@@ -1,36 +1,45 @@
 const SerialPort = require('serialport');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 const serialPort = new SerialPort('COM6', { baudRate: 9600 });
+
+app.use(cors());
 
 serialPort.on('open', () => {
   console.log('serial port open');
 });
 
 app.get('/write', (req, res) => {
-  // TODO: Wrap in try-catch for error handling
-  // TODO: return status codes
-  serialPort.write(`${req.query.message}\n`, (err) => {
-    if (err) {
-      return console.log('Error on write: ', err.message);
+  (async () => {
+    try {
+      await serialPort.write(`${req.query.message}\n`);
+      console.log('message written');
+      res.status(200).send();
+    } catch (error) {
+      res.status(400).send();
+      console.error('Error on write: ', error.message);
+      console.error(error);
     }
-    console.log('message written');
-  });
+  })();
 })
 
 app.get('/clear', (req, res) => {
-  // TODO: Wrap in try-catch for error handling
-  // TODO: return status codes
-  serialPort.write(' '.repeat(16), (err) => {
-    if (err) {
-      return console.log('Error on write: ', err.message);
+  (async () => {
+    try {
+      await serialPort.write(' ');
+      console.log('display cleared');
+      res.status(200).send();
+    } catch (error) {
+      res.status(400).send();
+      console.error('Error on write: ', error.message);
+      console.error(error);
     }
-    console.log('display cleared');
-  });
+  })();
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Backend listening on port ${port}`)
 })
